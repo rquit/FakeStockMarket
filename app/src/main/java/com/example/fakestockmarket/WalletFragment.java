4047862bbc7dd6a4351637494ea37460e134bebd
Andy Llerena <andyllerena28@gmail.com>
2022-05-03 22:33:27 -0400

    Worked on layout for Wallet Page. Attempted to work on buttons

--- app/src/main/java/com/example/fakestockmarket/WalletFragment.java
+++ app/src/main/java/com/example/fakestockmarket/WalletFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,5 +17,15 @@ public class WalletFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_wallet, container, false);
+
+        Button btn_deposit =(Button) findViewById(R.id.button_deposit);
+
+        btn_deposit.setOnClickListener (new View.onClickListener() {
+            @Override
+                    public void onClick(View v){
+
+            }
+        });
     }
-}
+    }
+
