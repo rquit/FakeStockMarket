0b5022da12cb5cb2b831d30840c7f76942b23b0e
rquit <nabilomi1@gmail.com>
2022-05-03 23:35:16 -0400

    made buttons work

--- app/src/main/java/com/example/fakestockmarket/WalletFragment.java
+++ app/src/main/java/com/example/fakestockmarket/WalletFragment.java
@@ -14,11 +14,26 @@ import androidx.fragment.app.Fragment;
 
 public class WalletFragment extends Fragment {
     Button btn_deposit;
+    Button btn_withdraw;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_wallet, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btn_deposit = requireView().findViewById(R.id.button_deposit);
+        btn_deposit.setOnClickListener(e -> {
+            // stuff
+        });
+
+        btn_withdraw = requireView().findViewById(R.id.button_withdraw);
+        btn_withdraw.setOnClickListener(e -> {
+            // stuff
+        });
     }
 }
 
